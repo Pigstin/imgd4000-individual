b034e73417c0a95f6cfbe9587943d8a356e75129
Pigstin <afcb1379@gmail.com>
2025-12-15 01:24:33 -0500

    first tutorial done, done under boids

---
+++ boids/README.md
@@ -0,0 +1,4 @@
+# Boids
+
+## Despite it's name, this directory is not for the Boids tech demo. It is for the tutorials.
+## Sorry >_>
---
+++ boids/Source/boids/boids.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "boids.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, boids, "boids" );
