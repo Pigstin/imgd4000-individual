2c9c5da347fee4f2898549031a6132cef3935188
Pigstin <afcb1379@gmail.com>
2025-12-16 15:16:50 -0500

    A*: restarted to use blueprints

--- aStar/Source/aStar/aStar.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "aStar.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, aStar, "aStar" );
