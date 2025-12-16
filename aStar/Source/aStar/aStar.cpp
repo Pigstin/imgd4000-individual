bf44d608a16c0f959957f18b0fa8eeb903aef040
Pigstin <afcb1379@gmail.com>
2025-12-15 21:21:40 -0500

    Added a* stuff

---
+++ aStar/Source/aStar/aStar.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "aStar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, aStar, "aStar" );
